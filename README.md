92846c13d4a2723ea110c8241e1b52ffd0f0ed08
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-05-08 23:37:12 +0600

    all commit

---
+++ README.md
@@ -0,0 +1,17 @@
+project details : I have used many things to create this application like
+tailwindcss daisyui react router and here are two json files which are about
+upcoming events and another file where I have put blog data which I have used
+dynamically in the json file in the upcoming section etc. and I have created
+three additional ones which will not be able to login and those who are logged
+in can go to get details and when they go to get details they are putting two
+options name and email there they can make bookings besides I have designed the
+login and registration page, I have made a dynamic title and from the challenge
+part I am creating a page called My Profile where the user can update the
+profile and I am creating a doggling system with the I button and I am doing
+another one with the password and hide it. That is the forget password. If the
+user forgets the password, he can forget the password. This is the over all
+project and projector. I have taken the different types of designs that need to
+be taken from different websites. And I have hidden the Firebase configurations
+through the ENV file which will not be visible in Git.
+
+package : 1| Swiper slider 1| react icon 2| toolTip 3| motion
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,88 @@
+import { createBrowserRouter } from 'react-router';
+import Root from '../Layouts/Root/Root';
+import Home from '../Pages/Home/Home';
+import About from '../Pages/About/About';
+import Profile from '../Pages/Profile/Profile';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Registerr/Register';
+import CardDetails from '../Components/CardDetails/CardDetails';
+import PrivateRoute from '../Provider/AuthProvider/PrivateRoute';
+import Loading from '../Components/Loading/Loading';
+import ResatPasswordPage from '../Pages/ResatPasswordPage/ResatPasswordPage';
+import Contact from '../Pages/Contacs/Contact';
+import Blogs from '../Pages/Blogs/Blogs';
+import ErrorPage from '../Pages/Error/ErrorPage';
+import ContactPage from '../Pages/Contacs/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        Component: Home,
+        loader: () => fetch('/eventData.json'),
+        hydrateFallbackElement: <Loading></Loading>,
+      },
+      {
+        path: '/profile',
+        element: (
+          <PrivateRoute>
+            <Profile></Profile>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/about',
+        element: (
+          <PrivateRoute>
+            <About></About>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/login',
+        Component: Login,
+      },
+      {
+        path: '/ResatPasswordPage',
+        Component: ResatPasswordPage,
+      },
+      {
+        path: '/register',
+        Component: Register,
+      },
+      {
+        path: '/blogs',
+        element: (
+          <PrivateRoute>
+            <Blogs></Blogs>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/contact',
+        element: (
+          <PrivateRoute>
+            <ContactPage></ContactPage>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/cardDetails/:id',
+        element: (
+          <PrivateRoute>
+            <CardDetails></CardDetails>
+          </PrivateRoute>
+        ),
+        loader: () => fetch('/eventData.json'),
+        hydrateFallbackElement: <Loading></Loading>,
+      },
+      {},
+    ],
+  },
+]);
+
+export default router;
